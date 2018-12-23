079bc5e2fd1f84ef9e3b199cc28cb702a12675fc
Madi <madelyn.e.helton@gmail.com>
2018-12-22 19:54:18 -0600

    adding letter.js

--- letter.js
+++ letter.js
@@ -11,43 +11,33 @@ let wordLength = [];
 let placeholder = wordLength.join(" ");
 
 
+  // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
 let Letter = function(value) {
 
-  this.letter = value;
+  // A string value to store the underlying character for the letter
+  this.value = value;
+  // A boolean value that stores whether that letter has been guessed yet
   this.guessed = false;
-  
-  this.result = function(result) {
-    if (this.letter === " ") {
-      this.guessed = true;
-      return " ";
-    } else {
-      if (this.guessed === true) {
-        return "_";
-      } else {
-        return this.letter;
-      };
-    };
-  };
-  this.print = function() {
 
-  },
-  console.log("-----Placeholder-----");
-  console.log(placeholder);
-  console.log("----Word------");
-  console.log(word);
-  console.log("----Input------");
-  console.log(input);
-  console.log("----------");
-
+  if (this.value === " ") {
+    this.guessed = true;
+  } 
 };
 
-Letter(input, word, placeholder);
-// A string value to store the underlying character for the letter
-
-// A boolean value that stores whether that letter has been guessed yet
-
 // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+Letter.prototype.print = function() {
 
-// A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+  if (this.guessed) {
+    return (' ' + this.value + ' ');    
+  }  
+  return (" _ ");
+};
+  // console.log("-----Placeholder-----");
+  // console.log(placeholder);
+  // console.log("----Word------");
+  // console.log(word);
+  // console.log("----Input------");
+  // console.log(input);
+  // console.log("----------");
 
 module.exports = Letter;
