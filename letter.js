054a780ee81a760c6c09c246cb0aaaa6573c080f
Madi <madelyn.e.helton@gmail.com>
2018-12-23 10:15:58 -0600

    changes to letter.js content

--- letter.js
+++ letter.js
@@ -1,16 +1,3 @@
-let input = process.argv[2];
-let wordArray = ["one", "two", "three"];
-
-let randomIndex = [Math.floor(Math.random() * wordArray.length)];
-let word = Array.from(wordArray[randomIndex]);
-
-let wordLength = [];
-  for (let i = 0; i < word.length; i++) {
-    wordLength.push('_');
-    };
-let placeholder = wordLength.join(" ");
-
-
   // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
 let Letter = function(value) {
 
@@ -25,7 +12,7 @@ let Letter = function(value) {
 };
 
 // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-Letter.prototype.print = function() {
+Letter.prototype.guess = function() {
 
   if (this.guessed) {
     return (' ' + this.value + ' ');    
--- word.js
+++ word.js
@@ -1,8 +1,27 @@
-// Word.js: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
-
 let Letter = require('./letter');
 
+let input = process.argv[2];
+let wordArray = ["one", "two", "three"];
+
+let randomIndex = [Math.floor(Math.random() * wordArray.length)];
+let word = Array.from(wordArray[randomIndex]);
+
+let placeholder = wordLength.join(" ");
+
+
+
+// Word.js: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
+let Word = function(value) {
+  this.value = value;
 // An array of new Letter objects representing the letters of the underlying word
+  this.wordLength = [];
+  for (let i = 0; i < word.length; i++) {
+    wordLength.push('_');
+    };
+
+
+}
+
 
 // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
 
