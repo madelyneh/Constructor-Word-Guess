08396e0a66266b2b83ae17e9ec94f46010b174b5
Madi <madelyn.e.helton@gmail.com>
2018-12-23 15:34:41 -0600

    adding complete letter.js

--- letter.js
+++ letter.js
@@ -1,30 +1,17 @@
-  // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-let Letter = function(value) {
+function Letter(value) {
 
-  // A string value to store the underlying character for the letter
   this.value = value;
-  // A boolean value that stores whether that letter has been guessed yet
   this.guessed = false;
 
   if (this.value === " ") {
     this.guessed = true;
-  } 
+  }
+  this.guess = function() {
+    if (this.guessed) {
+      return (' ' + this.value + ' ');  
+    }  
+    return (" _ ");
+  };
 };
-
-// A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-Letter.prototype.guess = function() {
-
-  if (this.guessed) {
-    return (' ' + this.value + ' ');    
-  }  
-  return (" _ ");
-};
-  // console.log("-----Placeholder-----");
-  // console.log(placeholder);
-  // console.log("----Word------");
-  // console.log(word);
-  // console.log("----Input------");
-  // console.log(input);
-  // console.log("----------");
 
 module.exports = Letter;
