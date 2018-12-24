1fdfca19c67fce22544f11cb989c04c782b1ac06
Madi <madelyn.e.helton@gmail.com>
2018-12-23 18:45:27 -0600

    the word constructor works.

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 // index.js: The file containing the logic for the course of the game, which depends on Word.js and:
 let Word = require('./word');
 let inquirer = require("inquirer")
-
 let wordArray = ["one", "two", "three", "four"];
 let currentWord = Array.from((wordArray.splice((Math.floor(Math.random() * wordArray.length)), 1))[0]);
 
-let wordLength = [];
-  for (let i = 0; i < currentWord.length; i++) {
-    wordLength.push('_');
-    };
-let placeholder = wordLength.join(" ");
+let wordOdj = new Word(currentWord);
+
+// let placeholder = wordLength.join(" ");
 
 let incorrectLetters = [];
 let correctLetters = [];
+let count = 0;
 
-let game = function(value) {
+let game = function() {
 
+  console.log("\n* Guess this word * \n-------------------\n" + wordOdj.placeholder() + "\n-------------------");
 
-}
+  if (count >= 4){
+    // game ends
+    console.log("end");
+    return;
+  }
+    inquirer.prompt({
+        name: "letter",
+        type: "text",
+        message: "Enter your guess.",
+    }).then(function(input) {
+      // console.log(input);
+      wordOdj.newGuess(input.letter);
+      count ++;
+      game();
+    });
+
+};
+
+game();
 
 // Randomly selects a word and uses the Word constructor to store it
 
--- word.js
+++ word.js
@@ -1,4 +1,4 @@
-let Letter = require('./letter').default;
+let Letter = require('./letter');
 
 let wordArray = ["one", "two", "three"];
 let randomIndex = [Math.floor(Math.random() * wordArray.length)];
@@ -9,23 +9,30 @@ let currentWord = Array.from(wordArray[randomIndex]);
 // Word.js: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
 let Word = function(value) {
   this.value = value;
-// An array of new Letter objects representing the letters of the underlying word
   this.letterObj = [];
+  for(let i = 0; i < value.length; i++) {
+    this.letterObj.push(new Letter(this.value[i]));
+    };
 
-  // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
-  this.newLetter = function(value){
-    this.letterObj.push(new Letter(value));
-    console.log("​Word -> newLetter", newLetter)
+  this.placeholder = function(){
+    let outcome = '';
+
+    for (let i=0; i < this.letterObj.length; i++) {
+      outcome += this.letterObj[i].guess();
+    } return outcome;
   };
 
-  // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js) 
-  this.newGuess = function(value) {
-    for (let i=0; i<letterObj.length; i++) {
-
-    }
-
+  this.newGuess = function(input) {
+    this.input = input;
+  
+    for (let i=0; i < this.letterObj.length; i++) {
+      if (this.letterObj[i].value === input) {
+        this.letterObj[i].guessed = true;
+        
+        console.log("Good job! " + input + " was a correct guess.");
+      }
+    } 
   };
-
 };
 
 
